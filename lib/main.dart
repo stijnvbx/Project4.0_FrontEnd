788865f27b87db789e93dbae0b6ed1ac24ebfb7a
lloydmoons <r0751957@student.thomasmore.be>
2021-02-04 15:58:24 +0100

    monteur kan alle de boxen zien met locatie

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:project4_front_end/pages/home.dart';
 import 'package:project4_front_end/pages/login.dart';
 import 'package:project4_front_end/pages/profile.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   // HttpOverrides.global = new MyHttpOverrides();
