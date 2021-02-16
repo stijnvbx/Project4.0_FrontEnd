37ed4733b906880014d7cf535413a55db1c68d87
Djorven Wielockx <r0740966@student.thomasmore.be>
2021-02-16 09:36:12 +0100

    Naaamveranderingen doorgestuurd

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:project4_front_end/pages/home.dart';
 import 'package:project4_front_end/pages/login.dart';
 import 'package:project4_front_end/pages/profile.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   // HttpOverrides.global = new MyHttpOverrides();
